7d921ac2a891747f734a131fd2a95df002df15d6
jules <jules.adikhanyan@gmail.com>
2021-03-14 21:01:29 +0300

    fix location

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(setPosition);
+function getPosition() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(setPosition);
+    }
 }
 
 function setPosition(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
-    weather.fetchWeather(latitude, longitude);
+    weather.fetchWeatherByCoords(latitude, longitude);
 }
 
 const weather = {
     "key": "a278fd3c83b4ffbf98780de2881d2e7d",
-    fetchWeather: function (latitude, longitude) {
-        let api = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude +
-        "&units=metric&appid=" + this.key;
 
+    fetchWeatherByCoords: function (latitude, longitude) {
+        let api = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude +
+            "&units=metric&appid=" + this.key;
         fetch(api).then((response) => response.json())
             .then((data) => this.displayWeather(data));
     },
+
+    fetchWeatherByName: function (city) {
+        let api = "http://api.openweathermap.org/data/2.5/weather?q=" + city +
+            "&units=metric&appid=" + this.key;
+        fetch(api).then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+
     displayWeather: function (data) {
         const { name } = data;
         const { icon } = data.weather[0];
@@ -27,21 +37,25 @@ const weather = {
         document.querySelector(".my-city h2").innerHTML = name;
         document.querySelector(".my-city img").src = "http://openweathermap.org/img/wn/" + icon + "@4x.png";
         document.querySelector(".my-city p").innerText = Math.round(temp) + "°C";
-        document.querySelector(".my-city-list .wind").innerText = speed + " m/s";
+        document.querySelector(".my-city-list .wind").innerText = Math.round(speed) + " m/s";
         document.querySelector(".my-city-list .pressure").innerText = pressure + " hpa";
         document.querySelector(".my-city-list .humidity").innerHTML = humidity + " %";
         document.querySelector(".my-city-list .feels_like").innerHTML = Math.round(feels_like) + "°C";
         document.querySelector(".my-city-list .coords").innerHTML = "[" + lon.toFixed(2) + ", " + lat.toFixed(2) + "]";
-    },
 
-    /*search: function () {
-        this.fetchWeather(document.querySelector(".search").value);
-    }*/
+        document.getElementById("wait").style.display = "none";
+        document.getElementById("current-city").style.display = "grid";
+        document.getElementById("favorites").style.display = "grid";
+    },
 };
 
-/*document.querySelector(".new-city button").addEventListener("click", function () {
-    weather.search();
-});*/
+function reloadGeo() {
+    getPosition();
+    document.getElementById("wait").style.display = "flex";
+    document.getElementById("current-city").style.display = "none";
+}
+
+
 
 
 
