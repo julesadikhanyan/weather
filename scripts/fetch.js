a62b3007fa423ad4317f092db29421fe7fe2ec79
jules <jules.adikhanyan@gmail.com>
2021-04-02 19:43:56 +0300

    lab2 fix

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -3,32 +3,39 @@ const key = "a278fd3c83b4ffbf98780de2881d2e7d";
 async function fetchWeatherByCoords(latitude, longitude) {
     let api = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude +
         "&units=metric&appid=" + key;
-    let data = await fetch(api);
-
-    if (data.status === 200) {
+    let data;
+    try {
+        data = await fetch(api);
         data = await data.json();
         printCurrentCity(data);
+    } catch (e) {
+        console.log(e);
     }
 }
 
 async function fetchWeatherByName(name, id) {
     let api = "http://api.openweathermap.org/data/2.5/weather?q=" + name +
         "&units=metric&appid=" + key;
-    let data = await fetch(api);
-
-    if (data.status === 200) {
+    let data;
+    try {
+        data = await fetch(api);
         data = await data.json();
         printFavoriteCity(data, id);
+    } catch (e) {
+        console.log(e);
     }
 }
 
 async function fetchWeatherByID(id) {
     let api = "http://api.openweathermap.org/data/2.5/weather?id=" + id +
         "&units=metric&appid=" + key;
-    let data = await fetch(api);
+    let data;
 
-    if (data.status === 200) {
+    try {
+        data = await fetch(api);
         data = await data.json();
         printCurrentCity(data, id);
+    } catch (e) {
+        console.log(e)
     }
 }
