5edd664d9b867a2f3fa6e915418288e00c2a69b1
jules <jules.adikhanyan@gmail.com>
2021-04-09 19:45:50 +0300

    lab2 fix

--- js/fetch.js
+++ js/fetch.js
@@ -2,13 +2,28 @@ let key = 'a278fd3c83b4ffbf98780de2881d2e7d';
 let url = `http://api.openweathermap.org/data/2.5/weather?units=metric&appid=${key}&`;
 
 async function fetchWeatherByCoords(latitude, longitude) {
-    return await fetch(`${url}lat=${latitude}&lon=${longitude}`).then(response => response.json()).catch(e => console.log(e));
+    try {
+        let response = await fetch(`${url}lat=${latitude}&lon=${longitude}`);
+        return response.json()
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 async function fetchWeatherByName(name) {
-    return await fetch(`${url}q=${name}`).then(response => response.json()).catch(e => console.log(e));
+    try {
+        let response = await fetch(`${url}q=${name}`);
+        return response.json()
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 async function fetchWeatherByID(id) {
-    return await fetch(`${url}id=${id}`).then(response => response.json()).catch(e => console.log(e));
+    try {
+        let response = await fetch(`${url}id=${id}`);
+        return response.json()
+    } catch (e) {
+        console.log(e)
+    }
 }
